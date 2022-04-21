d2da5bf64fc01fe6e570249f3782153700aa42f7
Vanshu <vanshugalhotra3332@gmail.com>
2022-04-21 17:55:43 +0530

    adding plus-minus quanity picker, singleProduct: frontend

--- Public/JS/script.js
+++ Public/JS/script.js
@@ -8,3 +8,23 @@ themeBtn.addEventListener('click', () => {
     element.classList.toggle('girl-theme');
 
 });
+
+// code for quantity picker: single_product.html
+
+const quantiy_input = document.getElementById('quantity'); // getting the quantity from input box
+const minusBtn = document.getElementById('minus-btn');
+const plusBtn = document.getElementById('plus-btn');
+
+var quantity = parseInt(quantiy_input.value); // converting the input value (string) to int
+
+plusBtn.addEventListener('click', () => {
+    quantity += 1;
+    quantiy_input.value = quantity;
+});
+
+minusBtn.addEventListener('click', () => {
+    if (quantity != 1) { // if quantity is already one, dont need to decrement it
+        quantity -= 1;
+        quantiy_input.value = quantity;
+    }
+});
