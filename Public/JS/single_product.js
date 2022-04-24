0f8ab70102f077ec3d7e62b00413b4f4a3931ade
Vanshu <vanshugalhotra3332@gmail.com>
2022-04-24 14:01:53 +0530

    added quick-view for product, product.html, frontend

--- Public/JS/single_product.js
+++ Public/JS/single_product.js
@@ -1,25 +1,3 @@
-
-// code for quantity picker: single_product.html
-
-const quantiy_input = document.getElementById('quantity'); // getting the quantity from input box
-const minusBtn = document.getElementById('minus-btn');
-const plusBtn = document.getElementById('plus-btn');
-
-var quantity = parseInt(quantiy_input.value); // converting the input value (string) to int
-
-plusBtn.addEventListener('click', () => {
-    quantity += 1;
-    quantiy_input.value = quantity;
-});
-
-minusBtn.addEventListener('click', () => {
-    if (quantity != 1) { // if quantity is already one, dont need to decrement it
-        quantity -= 1;
-        quantiy_input.value = quantity;
-    }
-});
-
-
 // getting the active wrapper items and opening and hiding the divs accordingly, like of description shipping etc..
 
 const wrap_aTags_and_Content = {
