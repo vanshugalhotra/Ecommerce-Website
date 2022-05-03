4d14f82144dc111f06ac92eb4f8e15e0aa011f29
Vanshu <vanshugalhotra3332@gmail.com>
2022-05-03 15:09:31 +0530

    added, getSingleProduct, updateProduct, deleteProduct: backend

--- controllers/products.js
+++ controllers/products.js
@@ -1,22 +1,42 @@
 const Products = require('../models/products'); // importing the products database model
 const StatusCodes = require('http-status-codes');
+const CustomError = require('../errors');
 
-const getAllProducts = async (req, res)=>{
-    const products = await(Products.find({}));
-    res.status(StatusCodes.OK).json({products, nbHits: products.length})
+const getAllProducts = async (req, res) => {
+    const products = await (Products.find({}));
+    res.status(StatusCodes.OK).json({ nbHits: products.length, products })
 }
-const createProduct = async (req, res)=>{
+const createProduct = async (req, res) => {
     const product = await Products.create(req.body);
     res.status(StatusCodes.CREATED).json({ product });
 }
-const getSingleProduct = async (req, res)=>{
+const getSingleProduct = async (req, res) => {
+    const { id: productID } = req.params;
+    const product = await Products.findOne({ _id: productID });
 
+    if (!product) {
+        throw new CustomError.NotFoundError(`No product with id : ${productID}`);
+    }
+    res.status(StatusCodes.OK).json({ product });
 }
-const updateProduct = async (req, res)=>{
+const updateProduct = async (req, res) => {
+    const { id: productID } = req.params;
+    const product = await Products.findOneAndUpdate({ _id: productID }, req.body,
+        { new: true, runValidators: true });
 
+    if (!product) {
+        throw new CustomError.NotFoundError(`No product with id : ${productID}`);
+    }
+    res.status(StatusCodes.OK).json({ product });
 }
-const deleteProduct = async (req, res)=>{
+const deleteProduct = async (req, res) => {
+    const { id: productID } = req.params;
+    const product = await Products.findOneAndRemove({ _id: productID });
 
+    if (!product) {
+        throw new CustomError.NotFoundError(`No product with id : ${productID}`);
+    }
+    res.status(StatusCodes.OK).json({deleted: true});
 }
 
 module.exports = {
